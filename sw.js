ac1c64493f82d1bc703761a541349892860aaef1
zulfadhliwza <zulfadhliwza@heitech.com.my>
2026-05-05 15:00:03 +0800

    modified:   dashboard.html 	modified:   index.html 	modified:   monitor.html 	modified:   sw.js

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@ const STATIC_ASSETS = [
     './offline.html',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
     'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css',
-    'https://code.jquery.com/jquery-4.0.0-beta.min.js',
+    'https://code.jquery.com/jquery-3.7.1.min.js',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
     'https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js',
     'https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js'
